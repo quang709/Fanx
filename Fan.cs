a883e46591c057bcdeb0a6a08594354f4c7013d7
quang709 <69231857+quang709@users.noreply.github.com>
2020-10-02 14:09:50 +0700

    Fanx

---
+++ Fan.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fan
+{
+    class Fan
+    {
+        const int SLOW = 1;
+        const int MEDIUM = 2;
+        const int FAST = 3;
+        private int speed = SLOW;
+        private bool on = false;
+        private double radius = 5;
+        private string color = "blue";
+        public Fan() { }
+        public int getSpeed()
+        {
+            return speed;
+        }
+        public void setSpeed(int newSpeed)
+        {
+            speed = newSpeed;
+        }
+        public bool isOn()
+        {
+            return on;
+        }
+        public void turnOn()
+        {
+            on = true;
+        }
+        public void turnOff()
+        {
+            on = false;
+        }
+        public double getRadius()
+        {
+            return radius;
+        }
+        public void setRadius(double newRadius)
+        {
+            radius = newRadius;
+        }
+        public string getColor()
+        {
+            return color;
+        }
+        public void setColor(string newColor)
+        {
+            color = newColor;
+        }
+        public string toString()
+        {
+            string Text = $"Speed: {speed}\nColor: {color}\nRadius: {radius}\n";
+            if (isOn())
+            {
+                Text += "Fan is on";
+            }
+            else Text += "Fan is off";
+            return Text;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fan fan1 = new Fan();
+            fan1.setColor("yellow");
+            fan1.setRadius(10);
+            fan1.setSpeed(1);
+            fan1.turnOn();
+            Fan fan2 = new Fan();
+            fan2.setColor("blue");
+            fan2.setRadius(5);
+            fan2.setSpeed(2);
+            fan2.turnOff();
+            Console.WriteLine(fan1.ToString());
+            Console.WriteLine(fan2.ToString());
+        }
+    }
+}
